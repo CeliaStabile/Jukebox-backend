407ace06633c7b46910e8efda53df7e5fdcc2332
thomas <bleszthomas@yahoo.fr>
2023-04-13 15:22:21 +0200

    routes put/delete queueItem

---
+++ routes/queue.js
@@ -0,0 +1,44 @@
+
+var express = require('express');
+var router = express.Router();
+require('../models/connection');
+const Party = require('../models/parties');
+
+
+
+// route pour ajouter une musique à la queueItem
+router.put('/new', function(req, res, next) {
+    const newQueueItem = {
+      title: req.body.title,
+      artist: req.body.artist,
+      url_image: req.body.url_image,
+      uri: req.body.uri,
+    };
+    Party.updateOne({name: req.query.name}, { $push: { queueItems: newQueueItem }})
+    .then(result => {
+      res.json({ success: true, message: 'Bien rajouté au queueItem' });
+    })
+    .catch(error => {
+      console.error(error);
+      res.json({ success: false, message: 'problème' });
+    });
+  });
+
+
+// route delete pour vider la queueItem
+  router.delete('/:name', function(req, res, next) {
+    Party.updateOne({ name: req.params.name }, { $set: { queueItems: [] } })
+      .then(data => {
+        res.json({ result: true, message: 'queueItem vidé' });
+      })
+      .catch(error => {
+        res.json({ result: false, error: 'erreur' });
+      });
+  });
+
+
+//   
+
+
+
+  module.exports = router;
